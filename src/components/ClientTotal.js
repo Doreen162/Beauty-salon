1e2ce994b3402378b3d0017a8a8e3ddcc7260a6f
https://github.com/Doreen162 <https://github.com/Doreen162>
2020-08-21 15:39:40 +0200

    project

--- src/components/ClientTotal.js
+++ src/components/ClientTotal.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class ClientTotal extends React.Component {
-  render () {
-    let total = 0
-    const services = this.props.services
+  render() {
+    let total = 0;
+    const services = this.props.services;
 
-    Object.keys(services).map(key => (
-      total = total + services[key].price
-    ))
+    Object.keys(services).map((key) => (total = total + services[key].price));
 
     return (
       <div>
-        <b>
-          ${total}
-        </b>
+        <b>R{total}</b>
       </div>
-    )
+    );
   }
 }
 
 ClientTotal.propTypes = {
-  services: PropTypes.object
-}
+  services: PropTypes.object,
+};
 
-export default ClientTotal
+export default ClientTotal;
