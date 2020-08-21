db43c6099fb0f8f3ef126790f0e3d29cfe4256dc
https://github.com/Doreen162 <https://github.com/Doreen162>
2020-08-21 14:48:43 +0200

    Updates

---
+++ precache-manifest.8293184c972f8aa600061b77c65aee86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64678db407d1ed01b3651316f50bc28",
+    "url": "/Doreen162/Beauty-salon/index.html"
+  },
+  {
+    "revision": "c9fd2d4316cde33a7bc5",
+    "url": "/Doreen162/Beauty-salon/static/css/main.2c649460.chunk.css"
+  },
+  {
+    "revision": "f4dd55fb096f0dd70235",
+    "url": "/Doreen162/Beauty-salon/static/js/2.8858725d.chunk.js"
+  },
+  {
+    "revision": "7e6ec0d9861ce35e695c4f042343a82f",
+    "url": "/Doreen162/Beauty-salon/static/js/2.8858725d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fd2d4316cde33a7bc5",
+    "url": "/Doreen162/Beauty-salon/static/js/main.fa1276e2.chunk.js"
+  },
+  {
+    "revision": "c791fba7ee320e58671e",
+    "url": "/Doreen162/Beauty-salon/static/js/runtime-main.d917e111.js"
+  }
+]);
